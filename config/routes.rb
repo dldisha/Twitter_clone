06be052de254d39a7072e82629ec7a03d412f241
dldisha <dldisha929@gmail.com>
2019-06-02 09:00:08 +0530

    added comments and some updates

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :tweets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "tweets#index"   #root_path..home page
+  root "tweets#index"   #home page
 end
